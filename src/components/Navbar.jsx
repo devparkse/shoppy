1d6209bbd917d7b892b4112c7a763cf38c25359f
Park Sieun <pmj8856@naver.com>
2023-07-15 00:01:11 +0900

    firebase 의 제품 정보 출력하기

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,25 +9,23 @@ import { useAuthContext } from "./context/AuthContext";
 export default function Navbar() {
   const { user, login, logout } = useAuthContext();
   return (
-    <div>
-      <header className="flex justify-between border-b border-gray-300 p-2">
-        <Link to="/" className="flex items-center text-4xl text-brand">
-          <FiShoppingBag />
-          <h1>Shoppy</h1>
-        </Link>
-        <nav className="flex items-center gap-4 font-semibold">
-          <Link to="/products">Products</Link>
-          {user && <Link to="/carts">Carts</Link>}
-          {user && user.isAdmin && (
-            <Link to="/products/new" className="text-2xl">
-              <BsFillPencilFill />
-            </Link>
-          )}
-          {user && <User user={user} />}
-          {!user && <Button text={"Login"} onClick={login} />}
-          {user && <Button text={"Logout"} onClick={logout} />}
-        </nav>
-      </header>
-    </div>
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {user && <User user={user} />}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
+      </nav>
+    </header>
   );
 }
